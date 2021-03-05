270fa8301fcb7e7cddc7c29d48cc94d5a3aea01a
David Taylor <instantink+pair+david@hp.com>
2021-03-05 15:02:20 -0800

    Update routes/permissions/home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admins
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   root 'index#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
