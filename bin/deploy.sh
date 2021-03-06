fc5457720314cdf4dcb96c2646eb7e6c18351bd5
David Taylor <instantink+pair+david@hp.com>
2021-03-05 16:04:21 -0800

    Update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,5 @@ set -e
 echo "Deploying Ping Pong Manager"
 
 heroku git:remote --app hp-ping-pong --remote heroku-20
-heroku run --remote heroku-18 rails db:migrate
-heroku run --remote heroku-18 rails db:seed
+heroku run --remote heroku-20 rails db:migrate
+heroku run --remote heroku-20 rails db:seed
